1df2cbfd5e65ba37986ee0dc163413881b2f52b9
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-04-29 15:17:45 -0400

    feat: config test unitario

---
+++ __test__/src/utils/hashPassword.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest';
+import { hashPassword } from '../../../src/utils/password-generator'; // Ajuste o caminho conforme necessário
+import bcrypt from 'bcrypt';
+
+describe('hashPassword', () => {
+  it('deve retornar um hash válido para a senha fornecida', async () => {
+    const senhaOriginal = 'minhaSenha123';
+    const hash = await hashPassword(senhaOriginal);
+
+    // Verifica se é uma string e se o hash bate com a senha original
+    expect(typeof hash).toBe('string');
+    const corresponde = await bcrypt.compare(senhaOriginal, hash);
+    expect(corresponde).toBe(true);
+  });
+});
